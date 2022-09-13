6081fc0f62a581412a8180f7fbd934ac45e1400e
Rodrigo Palomo <repmlm@hotmail.com>
2022-09-13 10:16:05 -0300

    first commit

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchFeed = () => {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
+
+export default SearchFeed
---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoDetail = () => {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
+
+export default VideoDetail
